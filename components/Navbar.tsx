d6e59cc69c66d6cc0245f9ba7774157267b3ae4d
kes-phyl <danphylbert@gmail.com>
2023-11-17 12:05:55 -0800

    feature: hero components

---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { CustomButton } from '.'
+
+const Navbar = () => {
+  return (
+    <header className='w-full absolute z-10'>
+        <nav className='max-w-[1440px] mx-auto flex justify-between item-center sm:px-16 px-6 py-4'>
+     <Link href='/' className='flex justify-center item-center'>
+        <Image 
+        src='/logo.svg' 
+        alt='Car show logo'
+        width={118}
+        height={180}
+        className='object-contain'     
+        />
+     </Link>
+     <CustomButton
+        title='Sign In'
+        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+        btnType = 'button'
+        />
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
