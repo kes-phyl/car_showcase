6202c4ff3d7fab02d18c827c705679a97a575c2b
kes-phyl <danphylbert@gmail.com>
2023-11-17 10:30:51 -0800

    add components and implement searchBar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,9 +26,10 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src='/hero.png' alt='hero' fill className='object-cotain'/>
-          <div className="hero__image-overlay"></div>
-        </div>
+          <Image src='/hero.png' alt='hero' fill className='object-contain'/>
+          </div>
+          <div className="hero__image-overlay"/>
+       
       </div>
     </div>
   )
