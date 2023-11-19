0210a96a27cccd086c25181941376235240faa82
kes-phyl <danphylbert@gmail.com>
2023-11-19 03:54:40 +0000

    implement card component,

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { CarProps } from '@/types'
+import { calculateCarRent } from '@/utils';
 
-const CarCard = () => {
+
+interface CarCardProps {
+  car: CarProps
+}
+
+
+
+const CarCard = ({car}: CarCardProps) => {
+
+  const {city_mpg, make, year, model, transmission, drive} = car;
+  const carRent = calculateCarRent(city_mpg, year)
+
   return (
-    <div>CarCard</div>
+    <div className='car-card group'>
+      <div className='car-card__content'>
+        <h2 className='car-card__title'>{make} {year}</h2>
+      </div>
+
+
+      <p className='flex mt-6 text-[32px] font-extrabold'>
+        <span className='self-start text-[14px] font-semibold'>$</span>
+        {carRent}
+        <span className='self-end text-[14px] font-semibold'>/day</span>
+      </p>
+    </div>
   )
 }
 
